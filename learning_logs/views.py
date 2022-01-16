a8fd38a5115f9381551bbd417fbf111bc06ef22d
Roger <51401112+Ishkrynn@users.noreply.github.com>
2022-01-17 00:18:40 +0100

    new commit

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -25,7 +25,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
-    topic = get_object_or_404(Topic, id=topic_id)
+    topic = Topic.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user.
     check_topic_owner(request, topic)
     entries = topic.entry_set.order_by('-date_added')
@@ -54,7 +54,7 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
-    topic = get_object_or_404(Topic, id=topic_id)
+    topic = topic = Topic.objects.get(id=topic_id)
     check_topic_owner(request, topic)
 
     if request.method != 'POST':
@@ -76,7 +76,7 @@ def new_entry(request, topic_id):
 @login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
-    entry = get_object_or_404(Entry, id=entry_id)
+    entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
     check_topic_owner(request, topic)
 
